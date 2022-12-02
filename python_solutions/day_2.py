ab330e9150527a17385dc08f890f272c1fdc9924
crnbaker <christian.baker@kcl.ac.uk>
2022-12-02 15:23:44 +0000

    day 2 part 2 (python) complete

--- python_solutions/day_2.py
+++ python_solutions/day_2.py
@@ -1,4 +1,5 @@
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
 from typing import List, Tuple
@@ -18,8 +19,8 @@ TOOL_SCORES = {
 
 class Outcome(Enum):
 	DRAW = 0
-	WIN = 1
-	LOSE = 2
+	LOSE = 1
+	WIN = 2
 
 OUTCOME_SCORES = {
 	Outcome.LOSE: 0,
@@ -33,11 +34,18 @@ OPPONENT_PLAY_CODE = {
 	'C': Tool.SCISSORS
 }
 
-MY_PLAY_CODE = {
+MY_PLAY_CODE_PART_1 = {
 	'X': Tool.ROCK,
 	'Y': Tool.PAPER,
 	'Z': Tool.SCISSORS
 }
+
+MY_PLAY_CODE_PART_2 = {
+	'X': Outcome.LOSE,
+	'Y': Outcome.DRAW,
+	'Z': Outcome.WIN
+}
+
 
 @dataclass
 class Round:
@@ -45,37 +53,67 @@ class Round:
 	my_play: Tool
 
 	def __str__(self) -> str:
-		return f'{self.opponent_play} vs {self.my_play}'
+		return f'{self.opponent_play} vs {self.my_play} ({self.play()})'
+
+	def play(self) -> int:
+		outcome = Outcome((self.opponent_play.value - self.my_play.value) % 3)
+		return OUTCOME_SCORES[outcome] + TOOL_SCORES[self.my_play]
 
 
-def load_strategy() -> List[Round]:
-	strategy = []
-	with open('../inputs/day_2.txt', 'r') as f:
-		for line in f.readlines():
-			if line != '\n':
-				coded_plays = line.split()[:2]
-				strategy.append(Round(
-					opponent_play=OPPONENT_PLAY_CODE[coded_plays[0]],
-					my_play=MY_PLAY_CODE[coded_plays[1]]
-					))
-	return strategy
+class Game(ABC):
+	def __init__(self) -> None:
+		self.rounds = self._load_rounds()
+
+	@abstractmethod
+	def _load_rounds(self) -> List[Round]:
+		raise NotImplementedError()
+
+	def play(self) -> int:
+		score = 0
+		for round in self.rounds:
+			score += round.play()
+		return score
 
 
-def play_round(round: Round) -> int:
-	
-	outcome = Outcome((round.my_play.value - round.opponent_play.value) % 3)
-	return OUTCOME_SCORES[outcome] + TOOL_SCORES[round.my_play]
+class Part1Game(Game):
+	def _load_rounds(self) -> List[Round]:
+		strategy = []
+		with open('../inputs/day_2.txt', 'r') as f:
+			for line in f.readlines():
+				if line != '\n':
+					coded_plays = line.split()[:2]
+					strategy.append(Round(
+						opponent_play=OPPONENT_PLAY_CODE[coded_plays[0]],
+						my_play=MY_PLAY_CODE_PART_1[coded_plays[1]]
+						))
+		return strategy
+
+
+class Part2Game(Game):
+	def _load_rounds(self) -> List[Round]:
+		strategy = []
+		with open('../inputs/day_2.txt', 'r') as f:
+			for line in f.readlines():
+				if line != '\n':
+					coded_plays = line.split()[:2]
+
+					opponent_play = OPPONENT_PLAY_CODE[coded_plays[0]]
+					desired_outcome = MY_PLAY_CODE_PART_2[coded_plays[1]]
+					my_play = Tool((opponent_play.value - desired_outcome.value) % 3)
+
+					strategy.append(Round(
+						opponent_play=opponent_play,
+						my_play=my_play
+						))
+		return strategy
 
 
 def main() -> None:
-	strategy = load_strategy()
-	score = 0
 
-	print(f'Playing {len(strategy)} rounds...')
-	for round in strategy:
-		this_score = play_round(round)
-		score += this_score
-	print(f'My total score would be {score} points')
+	game1 = Part1Game()
+	game2 = Part2Game()
+	print(f'My total score in game 1 would be {game1.play()} points')
+	print(f'My total score in game 2 would be {game2.play()} points')
 
 
 if __name__ == '__main__':
