c91948b591fe97761d28b2f895346d75cb7fbd78
crnbaker <christian.baker@kcl.ac.uk>
2022-12-02 22:51:27 +0000

    convert indentation to spaces

--- cpp_solutions/day_2.cpp
+++ cpp_solutions/day_2.cpp
@@ -7,63 +7,63 @@
 
 std::vector<Round> Game::loadRounds()
 {
-	std::ifstream file("../inputs/day_2.txt");
-	std::string line;
-	std::vector<Round> rounds;
-	while (std::getline(file, line)) {
-		if (isalpha(line[0])) {
-			std::vector<std::string> coded_round;
-			std::stringstream coded_line(line);
-			std::string coded_play;
-			while (std::getline(coded_line, coded_play, ' '))
-			{
-				coded_round.push_back(coded_play);
-			};
-			rounds.push_back(decodeRound(coded_round));
-		}
-	}
-	return rounds;
+    std::ifstream file("../inputs/day_2.txt");
+    std::string line;
+    std::vector<Round> rounds;
+    while (std::getline(file, line)) {
+        if (isalpha(line[0])) {
+            std::vector<std::string> coded_round;
+            std::stringstream coded_line(line);
+            std::string coded_play;
+            while (std::getline(coded_line, coded_play, ' '))
+            {
+                coded_round.push_back(coded_play);
+            };
+            rounds.push_back(decodeRound(coded_round));
+        }
+    }
+    return rounds;
 };
 
 int Game::play()
 {
-	std::vector<Round> rounds = loadRounds();
-	int score = 0;
-	for (Round & round : rounds) {
-    	score += round.play();
-	}
-	return score;
+    std::vector<Round> rounds = loadRounds();
+    int score = 0;
+    for (Round & round : rounds) {
+        score += round.play();
+    }
+    return score;
 }
 
 Round Part1Game::decodeRound(std::vector<std::string> codedRound)
 {
-	Tool opponentPlay = opponentPlayCode[codedRound[0]];
-	Tool myPlay = myPlayCode[codedRound[1]];
-	Round round(opponentPlay, myPlay);
-	return round;
+    Tool opponentPlay = opponentPlayCode[codedRound[0]];
+    Tool myPlay = myPlayCode[codedRound[1]];
+    Round round(opponentPlay, myPlay);
+    return round;
 };
 
 Round Part2Game::decodeRound(std::vector<std::string> codedRound)
 {
-	Tool opponentPlay = opponentPlayCode[codedRound[0]];
-	Outcome desiredOutcome = desiredOutcomeCode[codedRound[1]];
-	int toolValue = (((opponentPlay - desiredOutcome) % 3) + 3) % 3;
-	Tool myPlay = Tool(toolValue);
-	Round round(opponentPlay, myPlay);
-	return round;
+    Tool opponentPlay = opponentPlayCode[codedRound[0]];
+    Outcome desiredOutcome = desiredOutcomeCode[codedRound[1]];
+    int toolValue = (((opponentPlay - desiredOutcome) % 3) + 3) % 3;
+    Tool myPlay = Tool(toolValue);
+    Round round(opponentPlay, myPlay);
+    return round;
 };
 
 int Round::play()
 {
-	int outcomeValue = (((opponentPlay - myPlay) % 3) + 3) % 3;
-	Outcome outcome = Outcome(outcomeValue);
-	return OUTCOME_SCORES[outcome] + TOOL_SCORES[myPlay];
+    int outcomeValue = (((opponentPlay - myPlay) % 3) + 3) % 3;
+    Outcome outcome = Outcome(outcomeValue);
+    return OUTCOME_SCORES[outcome] + TOOL_SCORES[myPlay];
 }
 
 int main()
 {
-	Part1Game game1;
-	Part2Game game2;
-	std::cout << "Game 1 result is " << game1.play() << std::endl;
-	std::cout << "Game 2 result is " << game2.play() << std::endl;
+    Part1Game game1;
+    Part2Game game2;
+    std::cout << "Game 1 result is " << game1.play() << std::endl;
+    std::cout << "Game 2 result is " << game2.play() << std::endl;
 }
--- cpp_solutions/day_2.hpp
+++ cpp_solutions/day_2.hpp
@@ -3,75 +3,75 @@
 #include <string>
 
 enum Tool {
-	ROCK,
-	PAPER,
-	SCISSORS
+    ROCK,
+    PAPER,
+    SCISSORS
 };
 
 enum Outcome {
-	DRAW,
-	LOSE,
-	WIN
+    DRAW,
+    LOSE,
+    WIN
 };
 
 std::map<Tool, int> TOOL_SCORES = {
-	{Tool::ROCK, 1},
-	{Tool::PAPER, 2},
-	{Tool::SCISSORS, 3}
+    {Tool::ROCK, 1},
+    {Tool::PAPER, 2},
+    {Tool::SCISSORS, 3}
 };
 
 std::map<Outcome, int> OUTCOME_SCORES = {
-	{Outcome::LOSE, 0},
-	{Outcome::DRAW, 3},
-	{Outcome::WIN, 6}
+    {Outcome::LOSE, 0},
+    {Outcome::DRAW, 3},
+    {Outcome::WIN, 6}
 };
 
 class Round
 {
 public:
-	Round(Tool opponentPlay, Tool myPlay)
-		: opponentPlay(opponentPlay), myPlay(myPlay) {};
-	~Round() {};
-	int play();
-	Tool opponentPlay;
-	Tool myPlay;
+    Round(Tool opponentPlay, Tool myPlay)
+        : opponentPlay(opponentPlay), myPlay(myPlay) {};
+    ~Round() {};
+    int play();
+    Tool opponentPlay;
+    Tool myPlay;
 };
 
 class Game
 {
 public:
-	Game() {};
-	~Game() {};
-	int play();
-	std::map<std::string, Tool> opponentPlayCode = {
-		{"A", Tool::ROCK},
-		{"B", Tool::PAPER},
-		{"C", Tool::SCISSORS}
-	};
+    Game() {};
+    ~Game() {};
+    int play();
+    std::map<std::string, Tool> opponentPlayCode = {
+        {"A", Tool::ROCK},
+        {"B", Tool::PAPER},
+        {"C", Tool::SCISSORS}
+    };
 
 private:
-	std::vector<Round> loadRounds();
-	virtual Round decodeRound(std::vector<std::string> codedRound) = 0;
+    std::vector<Round> loadRounds();
+    virtual Round decodeRound(std::vector<std::string> codedRound) = 0;
 };
 
 class Part1Game: public Game
 {
 private:
-	Round decodeRound(std::vector<std::string> coded_round) override;
-	std::map<std::string, Tool> myPlayCode = {
-		{"X", Tool::ROCK},
-		{"Y", Tool::PAPER},
-		{"Z", Tool::SCISSORS}
-	};
+    Round decodeRound(std::vector<std::string> coded_round) override;
+    std::map<std::string, Tool> myPlayCode = {
+        {"X", Tool::ROCK},
+        {"Y", Tool::PAPER},
+        {"Z", Tool::SCISSORS}
+    };
 };
 
 class Part2Game: public Game
 {
 private:
-	Round decodeRound(std::vector<std::string> coded_round) override;
-	std::map<std::string, Outcome> desiredOutcomeCode = {
-		{"X", Outcome::LOSE},
-		{"Y", Outcome::DRAW},
-		{"Z", Outcome::WIN}
-	};
+    Round decodeRound(std::vector<std::string> coded_round) override;
+    std::map<std::string, Outcome> desiredOutcomeCode = {
+        {"X", Outcome::LOSE},
+        {"Y", Outcome::DRAW},
+        {"Z", Outcome::WIN}
+    };
 };
