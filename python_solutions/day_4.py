eb61e34ed8652349559e200393768aad03fe09ad
crnbaker <christian.baker@kcl.ac.uk>
2022-12-05 15:04:04 +0000

    python day 4 complete

--- python_solutions/day_4.py
+++ python_solutions/day_4.py
@@ -1,27 +1,30 @@
+from typing import Set
 
 
-if __name__ == '__main__':
+def decode_elf_set(coded_set: str) -> Set[int]:
+    start_stop = [int(s) for s in coded_set.split("-")]
+    return set(range(start_stop[0], start_stop[1] + 1))
 
-	overlap_counter = 0
-	with open("../inputs/day_4.txt", "r") as f:
-		for line in f.readlines():
-			elf_codes = line.split(',')[:2]
 
-			first_elf_start_stop = [int(s) for s in elf_codes[0].split('-')]
-			first_elf_start = first_elf_start_stop[0]
-			first_elf_stop = first_elf_start_stop[1]
+if __name__ == "__main__":
 
-			second_elf_start_stop = [int(s) for s in elf_codes[1].split('-')]
-			second_elf_start = second_elf_start_stop[0]
-			second_elf_stop = second_elf_start_stop[1]
+    fully_contained_counter = 0
+    overlap_counter = 0
 
-			first_elf_zones = set(range(first_elf_start, first_elf_stop + 1))
-			second_elf_zones = set(range(second_elf_start, second_elf_stop + 1))
+    with open("../inputs/day_4.txt", "r") as f:
+        for line in f.readlines():
+            elf_codes = line.split(",")[:2]
 
-			max_zone_length = max([len(first_elf_zones), len(second_elf_zones)])
+            first_elf_zones = decode_elf_set(elf_codes[0])
+            second_elf_zones = decode_elf_set(elf_codes[1])
 
-			if len(first_elf_zones | second_elf_zones) == max_zone_length:
-				overlap_counter += 1
+            max_zone_length = max([len(first_elf_zones), len(second_elf_zones)])
 
-	print(f'There are {overlap_counter} overlaps')
-			
+            if len(first_elf_zones | second_elf_zones) == max_zone_length:
+                fully_contained_counter += 1
+
+            if len(first_elf_zones & second_elf_zones):
+                overlap_counter += 1
+
+    print(f"There are {fully_contained_counter} fully contained section assignments.")
+    print(f"There are {overlap_counter} overlapping section assignments")
