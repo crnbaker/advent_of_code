18ca820ad98c0c6ab19017da3c4a270aeabde708
Hugh Hopper <hughhopper@xmos.com>
2022-12-05 20:06:07 +0000

    redo day_5

--- python_solutions/day_5.py
+++ python_solutions/day_5.py
@@ -1,122 +1,42 @@
-from dataclasses import dataclass
-import re
-from typing import List, Tuple
+from pathlib import Path
+import pandas as pd
 
-NUM_STACKS = 9
-INITIAL_MAX_HEIGHT = 8
-PROCEDURE_START_LINE = 10
-CHARS_PER_CRATE = 4
+# find the files
+this_script_path = Path(__file__)
+aoc_path = this_script_path.parents[1]
+day5_input_path = aoc_path / 'inputs' / 'day_5.txt'
 
-NUM_CRATES_KEYWORD = 'move'
-SOURCE_STACK_KEYWORD = 'from'
-DEST_STACK_KEYWORD = 'to'
-
-@dataclass
-class CraneCommand:
-	num_crates_to_move: int
-	source_stack: int
-	destination_stack: int
-
-	def __str__(self) -> str:
-		return (
-			f"Moving {self.num_crates_to_move} crates "
-			f"from stack {self.source_stack + 1} "
-			f"to stack {self.destination_stack + 1}."
-		)
+# read everything in with pandas
+stack_col_w = [4]*8 + [3]
+stack_input = pd.read_fwf(day5_input_path, nrows=8, header=None, widths=stack_col_w)
+moves_input = pd.read_csv(day5_input_path, skiprows=10, header=None, delim_whitespace=True)
+moves_input.drop([0,2,4], axis=1, inplace=True)
+moves_input.rename({1:'move', 3:'from', 5:'to'}, axis=1, inplace=True)
 
 
-@dataclass
-class Crane:
-	stacks: Tuple[List[str], ...]
+# convert stack input into list of lists
+ncols = stack_input.shape[1]
+nrows = stack_input.shape[0]
+stack_lists = []
+for m in range(ncols):
+	stack_lists.append([])
+	for n in range(nrows):
+		rev_idx = nrows - n - 1
+		crate = stack_input.iloc[rev_idx, m]
+		if isinstance(crate, float): # its only a float if its actually a nan - good import technique with pd.read_fwf
+			break
+		else:
+			stack_lists[-1].append(crate[1])
 
-	def execute_command(self, command: CraneCommand) -> None:
+# iteratre all the moves
+for n, row in moves_input.iterrows():
+	how_many_to_move = row['move']
+	col_to_move_from = row['from'] - 1
+	col_to_move_to = row['to'] - 1
+	for m in range(how_many_to_move):
+		stack_lists[col_to_move_to].append(stack_lists[col_to_move_from].pop())
 
-		picked_up_crates = [
-			self.stacks[command.source_stack].pop()
-			for _ in range(command.num_crates_to_move)
-		]
-
-		dest_stack = self.stacks[command.destination_stack]
-		dest_stack += picked_up_crates
-	
-	@property
-	def top_crates(self) -> List[str]:
-		_top_crates = []
-		for stack in self.stacks:
-			_top_crates.append(stack[-1])
-		return _top_crates
-
-	def __str__(self) -> str:
-		tallest_stack_height = max([len(stack) for stack in self.stacks])
-		output = ''
-		for n in range(tallest_stack_height):
-			row = ''
-			for stack in self.stacks:
-				try:
-					row += f'[{stack[tallest_stack_height - n - 1]}] '
-				except IndexError:
-					row += ' ' * CHARS_PER_CRATE
-			row += '\n'
-			output += row
-		output += ''.join([f' {n + 1}  ' for n in range(NUM_STACKS)]) + '\n'
-		output += ('_' * (CHARS_PER_CRATE * NUM_STACKS) + '\n')
-		return output
-
-
-def decode_stacks() -> Tuple[List[str], ...]:
-
-	stacks: Tuple[List[str], ...] = tuple([[] for _ in range(NUM_STACKS)])
-
-	with open("../inputs/day_5.txt", "r") as f:
-		for line in f.readlines()[:INITIAL_MAX_HEIGHT]:
-			for n, stack in enumerate(stacks):
-				crate = line[n * CHARS_PER_CRATE + 1]
-				if crate.isalpha():
-					stack.append(crate)
-
-		return stacks
-
-
-def get_crane_commands() -> List[CraneCommand]:
-	command_list = []
-	with open("../inputs/day_5.txt", "r") as f:
-		for line in f.readlines()[PROCEDURE_START_LINE:]:
-			command_list.append(parse_crane_command(line))
-	return command_list
-
-
-def extract_integer_after_keyword(command_string: str, keyword: str) -> int:
-	regex = re.compile(keyword + ' \d+')
-	return int(regex.findall(command_string)[0][len(keyword):])
-
-
-def parse_crane_command(command_string: str) -> CraneCommand:
-
-	num_crates = extract_integer_after_keyword(command_string, NUM_CRATES_KEYWORD)
-	source = extract_integer_after_keyword(command_string, SOURCE_STACK_KEYWORD)
-	dest = extract_integer_after_keyword(command_string, DEST_STACK_KEYWORD)
-
-	return CraneCommand(
-		num_crates_to_move=num_crates,
-		source_stack=source - 1,
-		destination_stack=dest - 1
-	)
-
-def main() -> None:
-	crane = Crane(decode_stacks())
-
-	print(crane)
-
-	commands = get_crane_commands()
-
-	for command in commands:
-		print(command)
-		crane.execute_command(command)
-		print(crane)
-
-	print(f"{len(commands)} commands executed:")
-	print(f"Top crates are {''.join(crane.top_crates)}")
-
-
-if __name__ == '__main__':
-	main()
+# they should be all sorted now
+# find out the top item from each stack
+top_crate_str = ''.join([x.pop() for x in stack_lists])
+print(top_crate_str)
