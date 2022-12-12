a813f0f9e081e067f5f0eb7676b499d5cb776c55
crnbaker <christian.baker@kcl.ac.uk>
2022-12-12 20:31:47 +0000

    Python day 11 part 1 complete

---
+++ python_solutions/day_11.py
@@ -0,0 +1,121 @@
+
+from dataclasses import dataclass
+from functools import partial
+from operator import add, mul
+from pathlib import Path
+import re
+from typing import Callable, List, Optional
+
+INPUT_FILE_PATH = Path("../inputs/day_11.txt")
+NUM_ROUNDS_PART_1 = 20
+NUM_ROUNDS_PART_2 = 10000
+
+
+class Monkey:
+
+    def __init__(
+        self,
+        initial_items: List[int],
+        operation: Callable[[int], int],
+        worry_divisor: int
+        ) -> None:
+
+        self._items = initial_items
+        self._operation: Callable[[int], int] = operation
+        self._worry_divisor = worry_divisor
+        self._get_target_monkey: Callable[[int], 'Monkey'] = lambda _ : self
+
+        self._num_items_inspected = 0
+
+    def set_target_monkey_function(self, func: Callable[[int], 'Monkey']) -> None:
+        self._get_target_monkey = func
+
+    def throw(self):
+        while self._items:
+            item_worry = self._items.pop(0)
+            item_worry = self._operation(item_worry) // self._worry_divisor
+            self._get_target_monkey(item_worry).catch(item_worry)
+            self._num_items_inspected += 1
+
+    def catch(self, item: int):
+        self._items.append(item)
+
+    @property
+    def num_items_inspected(self) -> int:
+        return self._num_items_inspected
+
+
+def extract_integer_after_keyword(command_string: str, keyword: str) -> int:
+    regex = re.compile(keyword + r" \d+")
+    return int(regex.findall(command_string)[0][len(keyword) :])
+
+
+def monkey_factory(spec: str, worry_divisor: int) -> Monkey:
+    items = spec.splitlines()[1].split(':')[1].strip().split(', ')
+
+    mult_matches = re.compile(r"\* \d+").findall(spec.splitlines()[2])
+    add_matches = re.compile(r"\+ \d+").findall(spec.splitlines()[2])
+    square_matches = re.compile(r"\* old").findall(spec.splitlines()[2])
+
+    if len(mult_matches) == 1:
+        operation = partial(mul, int(mult_matches[0].split()[1]))
+    elif len(add_matches) == 1:
+        operation = partial(add, int(add_matches[0].split()[1]))
+    elif len(square_matches) == 1:
+        operation = lambda x : x ** 2
+    else:
+        raise ValueError("Monkey's operation not recognised")
+
+    return Monkey(initial_items=[int(i) for i in items], operation=operation, worry_divisor=worry_divisor)
+
+
+def target_monkey_function(if_divisible_by: int, then_monkey: Monkey, else_monkey: Monkey, worry_level: int) -> Monkey:
+    if (worry_level % if_divisible_by) == 0:
+        return then_monkey
+    else:
+        return else_monkey
+
+
+def extract_integer_after_keyphrase(spec: str, keyword: str) -> int:
+    regex = re.compile(keyword + r" \d+")
+    return int(regex.findall(spec)[0][len(keyword) :])
+
+
+def target_monkey_function_factory(spec: str, monkeys: List[Monkey]) -> Callable[[int], 'Monkey']:
+    
+    if_divisible_by = extract_integer_after_keyphrase(spec, "Test: divisible by")
+    then_monkey_index = extract_integer_after_keyphrase(spec, "If true: throw to monkey")
+    else_monkey_index = extract_integer_after_keyphrase(spec, "If false: throw to monkey")
+
+    return partial(target_monkey_function, if_divisible_by, monkeys[then_monkey_index], monkeys[else_monkey_index])
+
+
+def load_monkeys(worry_divisor: int):
+    with open(INPUT_FILE_PATH, 'r') as f:
+        monkey_specs = f.read().split('\n\n')
+
+    monkeys = []
+    for monkey_spec in monkey_specs:
+        monkeys.append(monkey_factory(monkey_spec.strip(), worry_divisor))
+
+    for monkey, spec in zip(monkeys, monkey_specs):
+        monkey.set_target_monkey_function(target_monkey_function_factory(spec.strip(),  monkeys))
+
+    return monkeys
+
+
+
+if __name__ == '__main__':
+
+    part_1_monkeys = load_monkeys(worry_divisor=3)
+
+    for n in range(NUM_ROUNDS_PART_1):
+        for p1_monkey in part_1_monkeys:
+            p1_monkey.throw()
+
+    p1_num_inspections_per_monkey = []
+
+    for p1_monkey in part_1_monkeys:
+        p1_num_inspections_per_monkey.append(p1_monkey.num_items_inspected)
+
+    print(f"Part 1 monkey business: {sorted(p1_num_inspections_per_monkey)[-2] * sorted(p1_num_inspections_per_monkey)[-1]}")
